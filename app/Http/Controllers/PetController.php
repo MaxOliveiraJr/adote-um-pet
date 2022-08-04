6fd0992a1d4786faf1c8992af9deb5a917c060e1
Max Oliveira <max-oliveira@outlook.com.br>
2022-08-04 19:25:54 -0300

    feat: create model pets

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -2,24 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pet;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
-    public function index(){
-      return  [
-            [
-                'nome' => 'Max',
-                'sobrenome'=> 'Oliveira'
-            ],
-            [
-                'nome' => 'Max',
-                'sobrenome' => 'Oliveira'
-            ],
-            [
-                'nome' => 'Max',
-                'sobrenome' => 'Oliveira'
-            ],
-        ];
+    public function index()
+    {
+
+        /**
+         * Retorna lista de pets cadastrados
+         *
+         * @return Collection
+         */
+
+        return  Pet::get();
     }
 }
