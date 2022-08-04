8ecf1da972000a2f1c8b9fa23c9c105ca2c142df
Max Oliveira <max-oliveira@outlook.com.br>
2022-08-04 19:08:30 -0300

    feat: create controller complete

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
-    //
+    public function index(){
+      return  [
+            [
+                'nome' => 'Max',
+                'sobrenome'=> 'Oliveira'
+            ],
+            [
+                'nome' => 'Max',
+                'sobrenome' => 'Oliveira'
+            ],
+            [
+                'nome' => 'Max',
+                'sobrenome' => 'Oliveira'
+            ],
+        ];
+    }
 }
